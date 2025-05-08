56b8e9e39285599eb26cd161321c5c9e8d64967e
Eric9237 <k28828528@gmail.com>
2025-05-08 12:33:25 +0800

    5

--- my-site/server.js
+++ my-site/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path'); // ⬅️ 加這一行！
 
 const app = express();
-const port = 80;
+const port = 3000;
 
 // 靜態資料夾
 app.use(express.static('public'));
